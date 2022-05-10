28ebe587d4480864923f682ed55d83208f434ba9
Siwei Sun <siweisun@umich.edu>
2022-05-10 16:54:19 -0400

    functionality for adding unit to team implemented

--- rolldown.py
+++ rolldown.py
@@ -46,10 +46,12 @@ assert all([
 
 class Unit:
     """Class containing unit information."""
-    def __init__(self, cost, name, traits):
+    def __init__(self, cost, name, traits, level=1):
         self.cost = cost
         self.name = name
-        self.traits = traits
+        self.traits = [trait.strip() for trait in traits]
+
+        self.level = level
 
     def __str__(self):
         """String representation of a unit."""
@@ -57,8 +59,24 @@ class Unit:
 
     def __repr__(self):
         """Self representation of a unit."""
-        return self.name + ', ' + str(self.cost) + ', ' + ', '.join(self.traits) + '\n'
+        return str(self)
 
+    # This one is used by the __in__ builtin!
+    def __eq__(self, other):
+        """Compare units by name."""
+        if type(other) != Unit:
+            return False
+
+        # Check only name
+        return self.name == other.name
+
+    def unit_compare_level(self, other):
+        """Compare units by name and star level."""
+        return self.name == other.name and self.level == other.level
+
+    def upgrade(self):
+        """Return an upgraded version of the unit."""
+        return Unit(self.cost, self.name, self.traits, self.level + 1)
 
 
 class Trait:
@@ -77,9 +95,76 @@ class Trait:
 
     def __repr__(self):
         """Self representation of a trait."""
-        str_breaks = [str(breaks) for breaks in self.breakpoints]
-        str_styles = [str(styles) for styles in self.styles]
-        return self.name + ': ' + '/'.join(str_breaks) + ' ' + 'with styles ' + '/'.join(str_styles) + '\n'
+        return str(self)
+
+
+class Team:
+    """Class that represents a team of units."""
+    def __init__(self, champions_dict, traits_dict):
+        self.team = []
+        self.traits = {}
+
+        self.champions_dict = champions_dict
+        self.traits_dict = traits_dict
+
+    def __str__(self):
+        """The String representation of a team."""
+        str_team = [unit.name + ' ' + str(unit.level) + '\n' for unit in self.team]
+        str_traits = [str(trait) + ' ' + str(amount) for trait, amount in self.traits.items()]
+        units = "This is the current team:\n" + ''.join(str_team) + '\n'
+        traits = "Here are the current traits:\n" + '\n'.join(str_traits)
+        return units + traits
+
+    def __repr__(self):
+        """The self representation of a team."""
+        return str(self)
+
+    def add_unit(self, unit):
+        """Add unit to a team."""
+        # Error checking
+        assert type(unit) == str, "Error attempting to add unknown type to team."
+        try:
+            new_unit = self.champions_dict[unit]
+        except ValueError:
+            print("ValueError raised, check champion spelling.")
+            sys.exit()
+
+        # If it's a unique unit, add its traits to the team
+        if new_unit not in self.team:
+            for trait in new_unit.traits:
+                if trait not in self.traits:
+                    self.traits[trait] = 1
+                else:
+                    self.traits[trait] += 1
+            
+            # Finally, add the unit to the team
+            self.team.append(new_unit)
+
+        # If it's not a unique unit, check if it triggers an upgrade
+        else:
+            # We need to include the unit star level for this comparison
+            amount = sum([1 if unit.unit_compare_level(new_unit) else 0 for unit in self.team])
+
+            # If it triggers an upgrade, add the upgraded unit and remove the previous copies
+            if amount == 2:
+                # Remove previous copies
+                self.team = [unit for unit in self.team if not unit.unit_compare_level(new_unit)]
+
+                # Add upgraded copy
+                self.team.append(new_unit.upgrade())
+
+            # Otherwise, just add the unit
+            else:
+                self.team.append(new_unit)
+
+
+    def get_traits(self):
+        """Extract the activated traits from a team."""
+        activated_traits = []
+        for trait, amount in self.traits.enumerate():
+            activated_traits.append(trait, amount)
+
+        return activated_traits
 
 
 def read_database(input_dir):
@@ -93,7 +178,7 @@ def read_database(input_dir):
         traits_list = json.loads(traits_file.read())
 
     # Parse unit data
-    champions = []
+    champions = {}
     for champ in champions_list:
         # If champion has fewer than 2 traits, ignore it
         # Since it is a target dummy, voidspawn, tome, Veigar, etc.
@@ -101,10 +186,10 @@ def read_database(input_dir):
             continue
 
         # Add to champions list
-        champions.append(Unit(champ['cost'], champ['name'], champ['traits']))
+        champions[champ['name']] = Unit(champ['cost'], champ['name'], champ['traits'])
 
     # Parse trait data
-    traits = []
+    traits = {}
     for trait in traits_list:
         # Extract trait breakpoints and styles from trait data
         # TODO: Make exception for Rival
@@ -115,16 +200,22 @@ def read_database(input_dir):
             styles.append(breakpoint['style'])
 
         # Add to traits list
-        traits.append(Trait(trait['name'], breakpoints, styles))
+        traits[trait['name']] = Trait(trait['name'], breakpoints, styles)
 
     return champions, traits
 
 
 def main(input_dir):
     """Simulate a rolldown."""
+    # champions is dict of names to Units, traits is dict of names to Traits
     champions, traits = read_database(input_dir)
-    print(*champions, sep='')
-    print(*traits, sep='')
+    cur_team = Team(champions, traits)
+    cur_team.add_unit('Lux')
+    cur_team.add_unit('Lux')
+    cur_team.add_unit('Garen')
+    cur_team.add_unit('Lux')
+    cur_team.add_unit('Lux')
+    print(cur_team)
 
 
 if __name__ == '__main__':
