f68808f790dcc8f196b43acec561b8ec026fc9ee
Siwei Sun <siweisun@umich.edu>
2022-05-21 15:13:04 -0400

    rerolling works now

--- user_interface.py
+++ user_interface.py
@@ -18,8 +18,9 @@ from rolldown import Game
 
 
 # Scaled splash to fit window (X by Y)
-SCALED_SPLASH_SIZE = (300, 100)
-LABEL_ROW = SCALED_SPLASH_SIZE[0] + 70
+SCALED_SPLASH_SIZE = (380, 100)
+SPLASH_LOCATION = 400
+LABEL_ROW = SPLASH_LOCATION + 370
 LABEL_SIZE = (SCALED_SPLASH_SIZE[0], 60)
 RIGHT_ALIGN = LABEL_SIZE[0] - 20
 
@@ -28,19 +29,19 @@ class Menu(QMainWindow):
     """Main testing window."""
     def __init__(self):
         super().__init__()
+        # List of all displayed widgets
+        self.displays = []
+
+        # Window
         self.setWindowTitle("Rolldown")
 
-        current_roll = Game(sys.argv[1], 200, 10).roll()
+        self.display_shop()
 
-        # Display shop
-        for idx, unit in enumerate(current_roll):
-            self.display_unit(unit, idx * (SCALED_SPLASH_SIZE[0] + 1))
-            
         # Reroll button
         reroll = QLabel(self)
         reroll.resize(*SCALED_SPLASH_SIZE)
         reroll.setText('reroll')
-        reroll.move(500, 1000)
+        reroll.move(SPLASH_LOCATION, 1000)
         reroll.mousePressEvent = self.reroll
 
         self.resize(1006, 596 * 3)
@@ -50,11 +51,12 @@ class Menu(QMainWindow):
         """Display unit in the correct shop location."""
         # Image
         splash = QLabel(self)
-        splash_map = QPixmap(f'{sys.argv[1]}champions/{unit.id_name}.png'\
-            ).scaled(300, 300, Qt.KeepAspectRatio)
+        splash_map = QPixmap(f'{sys.argv[1]}champions/{unit.name}.png'\
+            ).scaled(SCALED_SPLASH_SIZE[0], SCALED_SPLASH_SIZE[0], Qt.KeepAspectRatio)
         splash.setPixmap(splash_map)
         splash.resize(*SPLASH_SIZE)
-        splash.move(col, 0)
+        splash.move(col, SPLASH_LOCATION)
+        splash.show()
 
         # Label background color (rarity)
         label_background = QLabel(self)
@@ -62,18 +64,21 @@ class Menu(QMainWindow):
         label_background.setPixmap(label_map)
         label_background.resize(*SCALED_SPLASH_SIZE)
         label_background.move(col, LABEL_ROW)
+        label_background.show()
 
         # Unit name
         label_name = QLabel(self)
         label_name.resize(*SCALED_SPLASH_SIZE)
         label_name.setText(f' {unit.name}')
         label_name.move(col, LABEL_ROW)
+        label_name.show()
 
         # Unit rarity
         label_rarity = QLabel(self)
         label_rarity.resize(*SCALED_SPLASH_SIZE)
         label_rarity.setText(f' {unit.cost}')
         label_rarity.move(col + RIGHT_ALIGN, LABEL_ROW)
+        label_rarity.show()
 
         # Make label and splash clickable
         label_background.mousePressEvent = self.buy_unit
@@ -81,15 +86,31 @@ class Menu(QMainWindow):
         label_rarity.mousePressEvent = self.buy_unit
         splash.mouseReleaseEvent = self.buy_unit
 
+        return label_background, label_name, label_rarity, splash
+
+    def display_shop(self):
+        """Display the current shop."""
+        current_roll = Game(sys.argv[1], 200, 10).roll()
+
+        # Display shop
+        for idx, unit in enumerate(current_roll):
+            self.displays.append(self.display_unit(unit, idx * (SCALED_SPLASH_SIZE[0] + 1)))
+
     def buy_unit(self, event):
         """Buy a unit."""
         print("CLICKED")
         print(event.globalX(), event.globalY())
-        
+
     def reroll(self, event):
         """Reroll the shop."""
-        QtCore.QCoreApplication.quit()
-        QtCore.QProcess.startDetached(sys.executable, sys.argv)
+        # Delete old shop
+        for disp in self.displays:
+            for widget in disp:
+                widget.deleteLater()
+
+        # Display new shop
+        self.displays = []
+        self.display_shop()
 
 
 if __name__ == '__main__':
